300a90b42f38d44227d4604d6ad2db7120b50384
fu2hito <fu2hito@proton.me>
2024-06-25 22:06:39 +0900

    default keymap削除
    当リポジトリの目的はvial対応のPoCであるため不要と判断

--- firmware/qmk/nubkey_module_vial/keymaps/default/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 takashicompany (@takashicompany)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#pragma once
-
-// #define POINTING_DEVICE_ROTATION_270
-#define DYNAMIC_KEYMAP_LAYER_COUNT 1
-#define PIMORONI_TRACKBALL_SCALE 1
-#define POINTING_DEVICE_TASK_THROTTLE_MS 5
