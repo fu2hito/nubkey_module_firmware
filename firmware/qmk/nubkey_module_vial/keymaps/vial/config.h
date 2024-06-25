f712d928581a80c9831db081d1542005b2ea4b9b
fu2hito <fu2hito@proton.me>
2024-06-25 22:07:13 +0900

    リファクタリング

--- firmware/qmk/nubkey_module_vial/keymaps/vial/config.h
+++ firmware/qmk/nubkey_module_vial/keymaps/vial/config.h
@@ -2,7 +2,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-// #define POINTING_DEVICE_ROTATION_270
-#define DYNAMIC_KEYMAP_LAYER_COUNT 1
-#define PIMORONI_TRACKBALL_SCALE 1
-#define POINTING_DEVICE_TASK_THROTTLE_MS 5
+#define VIAL_KEYBOARD_UID {0x25, 0xB9, 0x38, 0x4B, 0xE7, 0x3F, 0x4C, 0x52}
--- firmware/qmk/nubkey_module_vial/rules.mk
+++ firmware/qmk/nubkey_module_vial/rules.mk
@@ -1,1 +1,2 @@
-VIAL_INSECURE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
